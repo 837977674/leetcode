4a7981296aada24e93c01b4ce5b1fd926ecc1cf4
zhoutianpeng <ztp@apache.org>
2020-06-29 21:03:12 +0800

    init

---
+++ src/main/java/twosum/ZTP.java
@@ -0,0 +1,26 @@
+package twosum;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+public class ZTP {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
