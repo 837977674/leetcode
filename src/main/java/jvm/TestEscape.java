c640cf9df562eb6f9cce2f07e5d4812318e281a9
Temple Zhou <dbaztp@gmail.com>
2020-06-30 22:57:23 +0800

    增加 jit 逃逸测试

--- src/main/java/jvm/TestEscape.java
+++ src/main/java/jvm/TestEscape.java
@@ -5,7 +5,7 @@ package jvm;
 public class TestEscape {
     public static void main(String[] args) {
         long a1 = System.currentTimeMillis();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 10000000; i++) {
             alloc();
         }
         // 查看执行时间
@@ -13,7 +13,7 @@ public class TestEscape {
         System.out.println("cost " + (a2 - a1) + " ms");
         // 为了方便查看堆内存中对象个数，线程sleep
         try {
-            Thread.sleep(100000);
+            Thread.sleep(1000000);
         } catch (InterruptedException e1) {
             e1.printStackTrace();
         }
