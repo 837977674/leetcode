8ba3f10c6543190041ee80a61649e5df789f01f0
Temple Zhou <dbaztp@gmail.com>
2020-07-01 21:36:29 +0800

    fix reorder

--- src/main/java/jvm/TestReorder.java
+++ src/main/java/jvm/TestReorder.java
@@ -1,27 +1,43 @@
 package jvm;
 
 public class TestReorder {
-    static int x = 0, y = 0;
-    static int a = 0, b = 0;
+    /** 这是一个验证结果的变量 */
+    private static int a=0;
+    /** 这是一个标志位 */
+    private static boolean flag=false;
 
     public static void main(String[] args) throws InterruptedException {
-        Thread one = new Thread(new Runnable() {
-            public void run() {
-                a = 1;
-                x = b;
-            }
-        });
+        //由于多线程情况下未必会试出重排序的结论,所以多试一些次
+        for(int i=0;i<1000;i++){
+            ThreadA threadA=new ThreadA();
+            ThreadB threadB=new ThreadB();
+            threadA.start();
+            threadB.start();
 
-        Thread other = new Thread(new Runnable() {
-            public void run() {
-                b = 1;
-                y = a;
+            //这里等待线程结束后,重置共享变量,以使验证结果的工作变得简单些.
+            threadA.join();
+            threadB.join();
+            a=0;
+            flag=false;
+        }
+    }
+
+    static class ThreadA extends Thread{
+        public void run(){
+            a=1;
+            flag=true;
+        }
+    }
+
+    static class ThreadB extends Thread{
+        public void run(){
+            if(flag){
+                a *= 10;
             }
-        });
-        one.start();
-        other.start();
-        one.join();
-        other.join();
-        System.out.println("(" + x + "," + y + ")");
+            if (a==0) {
+                // 跑10次一定能触发指令重排序
+                System.out.println("ha,a==0");
+            }
+        }
     }
 }
