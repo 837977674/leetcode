d3b8e01932fae86f17f14d085d58f312d31a0b2c
wangxinhua <837977674@qq.com>
2020-07-06 22:18:25 +0800

    caigou wxh

--- src/main/java/longestprefix/wxh/Solution.java
+++ src/main/java/longestprefix/wxh/Solution.java
@@ -2,9 +2,27 @@ package longestprefix.wxh;
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if (strs.length==0) return "";
+
         String a=strs[0];
-
-
+        char[] b=a.toCharArray();
+        int i=0;
+        String c = "";
+        String d="";
+        while (i<b.length)
+        {
+            c=c.concat(String.valueOf(b[i]));
+            int j=0;
+            while(j<strs.length)
+            {
+                if (strs[j].startsWith(c))
+                {j++;}
+                else return d;
+            }
+            i++;
+            d=c;
+        }
+        return d;
 
     }
 }
---
+++ src/test/java/longestprefix/wxh/SolutionTest.java
@@ -0,0 +1,15 @@
+package longestprefix.wxh;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void longestCommonPrefix() {
+        String[] str={"d"};
+        //String[] str={};
+        System.out.println(new longestprefix.wxh.Solution().longestCommonPrefix(str));
+    }
+}
