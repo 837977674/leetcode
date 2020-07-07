c191c5b37fcbcfc3844fbd0841cf22dc0fe8f14c
zhoutianpeng <ztp@apache.org>
2020-07-07 22:19:26 +0800

    add caigou app

---
+++ src/main/java/caigou/service/CaigouApplication.java
@@ -0,0 +1,13 @@
+package caigou.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaigouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaigouApplication.class, args);
+    }
+    
+}
