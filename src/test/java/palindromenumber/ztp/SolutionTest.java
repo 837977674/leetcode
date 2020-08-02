f98a3acc8b830ed9623dc2ce879e343cea99777d
Temple Zhou <dbaztp@gmail.com>
2020-08-02 14:47:23 +0800

    fix

--- src/test/java/palindromenumber/ztp/SolutionTest.java
+++ src/test/java/palindromenumber/ztp/SolutionTest.java
@@ -8,5 +8,13 @@ public class SolutionTest {
 
     @Test
     public void isPalindrome() {
+        System.out.println(new Solution().isPalindrome(2222));
+        System.out.println(new Solution().isPalindrome(12321));
+        System.out.println(new Solution().isPalindrome(1221));
+        System.out.println(new Solution().isPalindrome(123221));
+        System.out.println(new Solution().isPalindrome(12));
+        System.out.println(new Solution().isPalindrome(10));
+        System.out.println(new Solution().isPalindrome(1));
+        System.out.println(new Solution().isPalindrome(0));
     }
 }
